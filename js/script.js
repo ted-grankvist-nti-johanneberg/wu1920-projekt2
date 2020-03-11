d7f9df5ca6f76d32673b96269f77c2b5f519983a
Ted <60172917+ted-grankvist-nti-johanneberg@users.noreply.github.com>
2020-03-11 10:49:03 +0100

    Literally nothing
    -Had a lot of bugs come up that I had
    to take care of, but i copied some code to make a couple of changes.

--- js/script.js
+++ js/script.js
@@ -1,19 +1,49 @@
 window.onscroll = function() {myFunction()};
-const block = document.querySelector('article.block')
+/* window.onload = function() {typeWriter()};
+const block = document.querySelector(".block")
 
 const tl = new TimelineMax();
 
-tl.fromTo(block,1, {width: "0vmin"}, {width: "70vmin"}) //Något fungerar inte, kolla på videon av Dev Ed igen och se ifall du gör något fel.
+tl.fromTo(block, 1, {width: "0%"}, {width: "70vmin"}, ease, Power2.easeInOut ) */ //Något fungerar inte, kolla på videon av Dev Ed igen och se ifall du gör något fel.
 
 function myFunction() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
+  
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    document.getElementById("knapp1").style.display = "block";
+  } else {
+    document.getElementById("knapp1").style.display = "none";
+  }
 }
 
+/*
+var i = 0;
+var txt = 'Lorem ipsum dummy text blabla.';
+var speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("changethis").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+} */
+
+
+/* window.onscroll = function() {rotateFunction()};
+
+function rotateFunction() {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    document.getElementById("knapp1").style.display = "block";
+  } else {
+    document.getElementById("knapp1").style.display = "none";
+  }
+} */
 
 // https://www.youtube.com/watch?v=4uL9bAKOaVQ
 
-// Skapa en animation för den lilla arrowen somm gör att den rotera 180 grader efter du scrollat x grader ner, och sedan när du scrollar upp igen förbi den punkten
-// på y-axeln så roterar den runt 180 grader tillbaka.
+/*Skapa en animation för den lilla arrowen somm gör att den rotera 180 grader efter du scrollat x grader ner, och sedan när du scrollar upp igen förbi den punkten
+på y-axeln så roterar den runt 180 grader tillbaka. */

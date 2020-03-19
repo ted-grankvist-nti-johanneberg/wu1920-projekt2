7a8f0754e3c7895b1cd24cb96e4609715036d5dc
Ted <60172917+ted-grankvist-nti-johanneberg@users.noreply.github.com>
2020-03-20 00:43:28 +0100

    -Final polish + more
    -Polished the site with some box-shadows as well as other stuff.
    
    -Tidying up some small code, further changes are to come.
    
    -Added a slide down modal to all pages.

--- js/script.js
+++ js/script.js
@@ -20,6 +20,31 @@ function myFunction() {
 
 }
 
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+function modalactivate() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+
 function changetoindex() {
   window.location.href='index.html';
 }
@@ -69,11 +94,6 @@ function fadetosakringar() {
   fullpage.addClass("pagefade");
   setTimeout(changetosakringar, 700);
 }
-
-
-
-
-
 
 
 var lastScrollTop = 0;

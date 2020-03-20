df00b83b05ddcdc1103f10441f82d229f3e22f37
Ted <60172917+ted-grankvist-nti-johanneberg@users.noreply.github.com>
2020-03-20 23:03:23 +0100

    Final touches
    -Final touches
    -Changed h1 and h2 color

--- js/script.js
+++ js/script.js
@@ -11,32 +11,21 @@ function myFunction() {
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
-
-  /* if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-    document.getElementById("knapp1").style.display = "block";  och här 
-   } else {
-    document.getElementById("knapp1").style.display = "none";  Skriv här din kod för att rotera din ikon 
-  }  */
-
 }
 
-// Get the modal
+
 var modal = document.getElementById("myModal");
 
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
 function modalactivate() {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
